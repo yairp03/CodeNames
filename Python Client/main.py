ddbc3ad533d189f57575890c3c973daf07c503b0
yairp <yairperetz03@gmail.com>
2021-02-15 00:26:47 +0200

    Basic client in python added

---
+++ Python Client/main.py
@@ -0,0 +1,16 @@
+import socket
+
+
+def main():
+    sock = socket.socket()
+    sock.connect(("localhost", 1234))
+    while True:
+        c, s = input("Enter message: ").split(" ")
+        msg = c + str(len(s)).rjust(5, "0") + s
+        if msg:
+            sock.send(msg.encode())
+            print(sock.recv(1024))
+
+
+if __name__ == "__main__":
+    main()
